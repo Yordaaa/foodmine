2b13038f8c1644bb26ff03f8b15481407e452b69
Yordanos tibebu <yordanostibeu5@gmail.com>
2023-11-13 12:33:34 -0800

    Added Cart service and cart page

---
+++ frontend/src/app/shared/models/CartItem.ts
@@ -0,0 +1,8 @@
+import { Food } from "./Food";
+
+export class CartItem{
+    constructor(public food:Food){
+    }
+quantity:number = 1;
+price :number = this.food.price;
+}
--- frontend/src/data.ts
+++ frontend/src/data.ts
@@ -1,10 +1,9 @@
-import {Food} from './app/shared/models/Food';
+import { Food } from './app/shared/models/Food';
 import { Tag } from './app/shared/models/Tag';
-
 
 export const sample_foods: Food[] = [
   {
-    id:'1',
+    id: '1',
     name: 'Pizza Pepperoni',
     cookTime: '10-20',
     price: 10,
@@ -15,7 +14,7 @@ export const sample_foods: Food[] = [
     tags: ['FastFood', 'Pizza', 'Lunch'],
   },
   {
-    id:'2',
+    id: '2',
     name: 'Meatball',
     price: 20,
     cookTime: '20-30',
@@ -26,7 +25,7 @@ export const sample_foods: Food[] = [
     tags: ['SlowFood', 'Lunch'],
   },
   {
-    id:'3',
+    id: '3',
     name: 'Hamburger',
     price: 5,
     cookTime: '10-15',
@@ -37,7 +36,7 @@ export const sample_foods: Food[] = [
     tags: ['FastFood', 'Hamburger'],
   },
   {
-    id:'4',
+    id: '4',
     name: 'Fried Potatoes',
     price: 2,
     cookTime: '15-20',
@@ -48,7 +47,7 @@ export const sample_foods: Food[] = [
     tags: ['FastFood', 'Fry'],
   },
   {
-    id:'5',
+    id: '5',
     name: 'Chicken Soup',
     price: 11,
     cookTime: '40-50',
@@ -59,19 +58,19 @@ export const sample_foods: Food[] = [
     tags: ['SlowFood', 'Soup'],
   },
   {
-    id:'6',
-    name: 'Vegetables Pizza',
+    id: '6',
+    name: 'Beyaynet',
     price: 9,
     cookTime: '40-50',
     favorite: false,
-    origins: ['italy'],
+    origins: ['Ethiopia'],
     stars: 4.0,
-    imageUrl: 'assets/food-6.jpg',
-    tags: ['FastFood', 'Pizza', 'Lunch'],
+    imageUrl: 'assets/food-7.jpg',
+    tags: ['FastFood', 'Lunch'],
   },
-]
- 
-export const sample_tags:Tag[] = [
+];
+
+export const sample_tags: Tag[] = [
   { name: 'All', count: 6 },
   { name: 'FastFood', count: 4 },
   { name: 'Pizza', count: 2 },
@@ -80,4 +79,4 @@ export const sample_tags:Tag[] = [
   { name: 'Hamburger', count: 1 },
   { name: 'Fry', count: 1 },
   { name: 'Soup', count: 1 },
-]
+];

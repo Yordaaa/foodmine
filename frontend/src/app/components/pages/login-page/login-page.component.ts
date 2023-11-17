9a4045e4ba2237136ffdee31c8424bfa286b104a
Yordanos tibebu <yordanostibebu5@gmail.com>
2023-11-17 11:43:04 -0800

    Added Logout functionality and additional login features

--- frontend/src/app/components/pages/login-page/login-page.component.ts
+++ frontend/src/app/components/pages/login-page/login-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login-page',
@@ -11,12 +13,8 @@ export class LoginPageComponent implements OnInit {
 
   loginForm! : FormGroup;
   isSubmitted = false;
-
-
-
-
-  constructor(private formBuilder:FormBuilder){
-
+  returnUrl = '';
+  constructor(private formBuilder:FormBuilder ,private userService:UserService, private activatedRoute:ActivatedRoute,private router:Router) {
   }
 
   ngOnInit(): void{
@@ -24,6 +22,7 @@ export class LoginPageComponent implements OnInit {
       email:['', [Validators.required, Validators.email]],
       password:['', Validators.required]
     });
+    this.returnUrl = this.activatedRoute.snapshot.queryParams.returnUrl;
    
    }
    get fc(){
@@ -33,7 +32,10 @@ submit(){
   this.isSubmitted = true;
   if(this.loginForm.invalid) return;
 
-  alert(`email: ${this.fc.email.value},
-  password: ${this.fc.password.value}` )
+  this.userService.login({
+    email:this.fc.email.value,
+    password:this.fc.password.value}).subscribe(() =>{
+      this.router.navigateByUrl(this.returnUrl);
+    });
      }
 }
---
+++ frontend/src/app/services/user.service.ts
@@ -0,0 +1,80 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { USER_LOGIN_URL, USER_REGISTER_URL } from '../shared/constants/urls';
+import { IUserLogin } from '../shared/interfaces/IUserLogin';
+import { IUserRegister } from '../shared/interfaces/IUserRegister';
+import { User } from '../shared/models/User';
+
+const USER_KEY = 'User';
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private userSubject =
+  new BehaviorSubject<User>(this.getUserFromLocalStorage());
+  public userObservable:Observable<User>;
+  static userObservable: any;
+  constructor(private http:HttpClient, private toastrService:ToastrService) {
+    this.userObservable = this.userSubject.asObservable();
+  }
+
+  public get currentUser():User{
+    return this.userSubject.value;
+  }
+
+  login(userLogin:IUserLogin):Observable<User>{
+    return this.http.post<User>(USER_LOGIN_URL, userLogin).pipe(
+      tap({
+        next: (user) =>{
+          this.setUserToLocalStorage(user);
+          this.userSubject.next(user);
+          this.toastrService.success(
+            `Welcome to Foodmine ${user.name}!`,
+            'Login Successful'
+          )
+        },
+        error: (errorResponse) => {
+          this.toastrService.error(errorResponse.error, 'Login Failed');
+        }
+      })
+    );
+  }
+
+  register(userRegiser:IUserRegister): Observable<User>{
+    return this.http.post<User>(USER_REGISTER_URL, userRegiser).pipe(
+      tap({
+        next: (user) => {
+          this.setUserToLocalStorage(user);
+          this.userSubject.next(user);
+          this.toastrService.success(
+            `Welcome to the Foodmine ${user.name}`,
+            'Register Successful'
+          )
+        },
+        error: (errorResponse) => {
+          this.toastrService.error(errorResponse.error,
+            'Register Failed')
+        }
+      })
+    )
+  }
+
+
+  logout(){
+    this.userSubject.next(new User());
+    localStorage.removeItem(USER_KEY);
+    window.location.reload();
+  }
+
+  private setUserToLocalStorage(user:User){
+    localStorage.setItem(USER_KEY, JSON.stringify(user));
+  }
+
+  private getUserFromLocalStorage():User{
+    const userJson = localStorage.getItem(USER_KEY);
+    if(userJson) return JSON.parse(userJson) as User;
+    return new User();
+  }
+}

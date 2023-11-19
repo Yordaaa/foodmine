2c6cc2b8a853c4c2055bcf6667b975edeac8e627
Yordanos tibebu <yordanostibebu5@gmail.com>
2023-11-18 23:53:04 -0800

    Connected to mongodb

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,39 +1,26 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+
 import express from "express";
 import cors from "cors";
-import { sample_foods, sample_tags } from "./data";
-
+import foodRouter from "./routers/food.router";
+import userRouter from "./routers/user.router";
+import {dbConnect} from './configs/database.config';
+dbConnect();
 const app = express();
-app.use(
-  cors({
+app.use(express.json());
+app.use(cors({
     credentials: true,
-    origin: ["http://localhost:4200"],
+    origin: ["http://localhost:4200"]
   })
 );
 
-app.get("/api/foods", (req, res) => {
-  res.send(sample_foods);
-});
-app.get("/api/foods/search/:searchTerm", (req, res) => {
-  const searchTerm = req.params.searchTerm;
-  const foods = sample_foods.filter((food) =>
-    food.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-  res.send(foods);
-});
+app.use("/api/foods", foodRouter);
+app.use("/api/users",userRouter)
 
-app.get("/api/foods/tags", (req, res) => {
-  res.send(sample_tags);
-});
-app.get("/api/foods/tag/:tagName", (req, res) => {
-  const tagName = req.params.tagName;
-  const foods = sample_foods.filter((food) => food.tags?.includes(tagName));
-  res.send(foods);
-});
-app.get("/api/foods/:foodId", (req, res) => {
-  const foodId = req.params.foodId;
-  const food = sample_foods.find((food) => food.id == foodId);
-  res.send(food);
-});
+
+
 
 const port = 5000;
 app.listen(port, () => {
